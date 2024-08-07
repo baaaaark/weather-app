59fe301904265fc855af06520c114132654ee84c
Jason Oliver <Jason@Abbigails-MacBook-Pro.local>
2024-08-07 18:37:39 -0500

    Changes for Initial Deployment

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,22 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* Favicon */}
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+		<Html>
+			<Head>
+				{/* Favicon */}
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+		);
+	}
 }
 
 export default MyDocument;
