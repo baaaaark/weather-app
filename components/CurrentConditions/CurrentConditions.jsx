467fbe613977f3557df99f55decc8f100e575ca0
Jason Oliver <Jason@Abbigails-MacBook-Pro.local>
2024-08-09 12:51:50 -0500

    tab/spacing format fixes

--- components/CurrentConditions/CurrentConditions.jsx
+++ components/CurrentConditions/CurrentConditions.jsx
@@ -4,28 +4,28 @@ import styles from './CurrentConditions.module.css';
 //TODO: Create Icons for each of the following on this page: https://www.weather.gov/forecast-icons/
 
 const CurrentConditions = ({ data }) => {
-  if (!data || data.properties.periods.length === 0) {
-    return <p>No current conditions available.</p>;
-  }
+	if (!data || data.properties.periods.length === 0) {
+		return <p>No current conditions available.</p>;
+	}
 
-  const period = data.properties.periods[0];
+	const period = data.properties.periods[0];
 
-  return (
-    <div className={styles.currentConditionsContainer}>
-      <div>
-        <img src={period.icon} alt="weather icon" className={styles.icon} />
-      </div>
-      <div className={styles.conditionsTextContainer}>
-        <p className={styles.shortForecastText}>{period.shortForecast}</p>
-        <div>
-          <p className={styles.text}><b>Temperature:</b> {period.temperature}°{period.temperatureUnit}</p>
-          <p className={styles.text}><b>Relative Humidity:</b> {period.relativeHumidity?.value}%</p>
-          <p className={styles.text}><b>Dewpoint:</b> {(period.dewpoint.value * 1.8 + 32).toFixed(0)}°F</p>
-          <p className={styles.text}><b>Wind:</b> {period.windSpeed === '0 mph' ? 'Calm' : period.windSpeed} {period.windDirection}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.currentConditionsContainer}>
+			<div>
+				<img src={period.icon} alt="weather icon" className={styles.icon} />
+			</div>
+			<div className={styles.conditionsTextContainer}>
+				<p className={styles.shortForecastText}>{period.shortForecast}</p>
+				<div>
+					<p className={styles.text}><b>Temperature:</b> {period.temperature}°{period.temperatureUnit}</p>
+					<p className={styles.text}><b>Relative Humidity:</b> {period.relativeHumidity?.value}%</p>
+					<p className={styles.text}><b>Dewpoint:</b> {(period.dewpoint.value * 1.8 + 32).toFixed(0)}°F</p>
+					<p className={styles.text}><b>Wind:</b> {period.windSpeed === '0 mph' ? 'Calm' : period.windSpeed} {period.windDirection}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CurrentConditions;
--- components/WeatherGraph/WeatherGraph.jsx
+++ components/WeatherGraph/WeatherGraph.jsx
@@ -10,184 +10,190 @@ import styles from './WeatherGraph.module.css'
 Chart.register(...registerables);
 //TODO: Separate out graphs into appropriate categories (one graph for temp, one for percent-based values, etc.)
 const WeatherGraph = ({ data }) => {
-  const periods = data.properties.periods;
-  const labels = periods.map(period => new Date(period.startTime));
-  const temperatures = periods.map(period => period.temperature);
-  const dewPoints = periods.map(period => period.dewpoint.value * 1.8 + 32);
-  const precipitationChances = periods.map(period => period.probabilityOfPrecipitation.value);
-  const windSpeeds = periods.map(period => {
-    const match = period.windSpeed.match(/\d+/);
-    return match ? parseInt(match[0], 10) : 0;
-  });
+	const periods = data.properties.periods;
+	const labels = periods.map(period => new Date(period.startTime));
+	const temperatures = periods.map(period => period.temperature);
+	const dewPoints = periods.map(period => period.dewpoint.value * 1.8 + 32);
+	const precipitationChances = periods.map(period => period.probabilityOfPrecipitation.value);
+	const windSpeeds = periods.map(period => {
+		const match = period.windSpeed.match(/\d+/);
+		return match ? parseInt(match[0], 10) : 0;
+	});
 
-  const [dayRange, setDayRange] = useState(1);
+	const [dayRange, setDayRange] = useState(1);
 
-  const handleSliderChange = (event, newValue) => {
-    setDayRange(newValue);
-  };
+	const handleSliderChange = (event, newValue) => {
+		setDayRange(newValue);
+	};
 
-  const filteredLabels = labels.slice(0, dayRange * 24);
-  const filteredTemperatures = temperatures.slice(0, dayRange * 24);
-  const filteredDewPoints = dewPoints.slice(0, dayRange * 24);
-  const filteredPrecipitationChances = precipitationChances.slice(0, dayRange * 24);
-  const filteredWindSpeeds = windSpeeds.slice(0, dayRange * 24);
+	const filteredLabels = labels.slice(0, dayRange * 24);
+	const filteredTemperatures = temperatures.slice(0, dayRange * 24);
+	const filteredDewPoints = dewPoints.slice(0, dayRange * 24);
+	const filteredPrecipitationChances = precipitationChances.slice(0, dayRange * 24);
+	const filteredWindSpeeds = windSpeeds.slice(0, dayRange * 24);
 
-  const [datasetsVisibility, setDatasetsVisibility] = useState({
-    temperature: true,
-    dewPoint: true,
-    precipitation: true,
-    windSpeed: true,
-  });
+	const [datasetsVisibility, setDatasetsVisibility] = useState({
+		temperature: true,
+		dewPoint: true,
+		precipitation: true,
+		windSpeed: true,
+	});
 
-  const handleToggle = (dataset) => {
-    setDatasetsVisibility(prevState => ({
-      ...prevState,
-      [dataset]: !prevState[dataset],
-    }));
-  };
+	const handleToggle = (dataset) => {
+		setDatasetsVisibility(prevState => ({
+			...prevState,
+			[dataset]: !prevState[dataset],
+		}));
+	};
 
-  const dataConfig = {
-    labels: filteredLabels.map(date => format(date, 'EEE, MMM do h a')),
-    datasets: [
-      {
-        label: 'Temperature (°F)',
-        data: datasetsVisibility.temperature ? filteredTemperatures : [],
-        borderColor: 'red',
-        fill: false,
-        yAxisID: 'y-axis-1',
-      },
-      {
-        label: 'Dew Point (°F)',
-        data: datasetsVisibility.dewPoint ? filteredDewPoints : [],
-        borderColor: 'blue',
-        fill: false,
-        yAxisID: 'y-axis-1',
-      },
-      {
-        label: 'Precipitation Chance (%)',
-        data: datasetsVisibility.precipitation ? filteredPrecipitationChances : [],
-        borderColor: 'green',
-        fill: false,
-        yAxisID: 'y-axis-2',
-      },
-      {
-        label: 'Wind Speed (mph)',
-        data: datasetsVisibility.windSpeed ? filteredWindSpeeds : [],
-        borderColor: 'orange',
-        fill: false,
-        yAxisID: 'y-axis-2',
-      },
-    ],
-  };
+	const dataConfig = {
+		labels: filteredLabels.map(date => format(date, 'EEE, MMM do h a')),
+		datasets: [
+			{
+				label: 'Temperature (°F)',
+				data: datasetsVisibility.temperature ? filteredTemperatures : [],
+				borderColor: 'red',
+				fill: false,
+				yAxisID: 'y-axis-1',
+			},
+			{
+				label: 'Dew Point (°F)',
+				data: datasetsVisibility.dewPoint ? filteredDewPoints : [],
+				borderColor: 'blue',
+				fill: false,
+				yAxisID: 'y-axis-1',
+			},
+			{
+				label: 'Precipitation Chance (%)',
+				data: datasetsVisibility.precipitation ? filteredPrecipitationChances : [],
+				borderColor: 'green',
+				fill: false,
+				yAxisID: 'y-axis-2',
+			},
+			{
+				label: 'Wind Speed (mph)',
+				data: datasetsVisibility.windSpeed ? filteredWindSpeeds : [],
+				borderColor: 'orange',
+				fill: false,
+				yAxisID: 'y-axis-2',
+			},
+		],
+	};
 
-  const options = {
-    scales: {
-      x: {
-        display: true,
-        title: {
-          type: 'time',
-          font: {
-            size: 24,
-          },
-        },
-        ticks: {
-          font: {
-            size: 18,
-          },
-          maxTicksLimit: Math.ceil(filteredLabels.length),
-        },
-      },
-      
-      'y-axis-1': {
-        type: 'linear',
-        display: true,
-        position: 'left',
-        title: {
-          display: true,
-          text: 'Temperature / Dew Point (°F)',
-          font: {
-            size: 24,
-          },
-        },
-        ticks: {
-          font: {
-            size: 18,
-          },
-        },
-      },
-      'y-axis-2': {
-        type: 'linear',
-        display: true,
-        position: 'right',
-        title: {
-          display: true,
-          text: 'Precipitation (%) / Wind Speed (mph)',
-          font: {
-            size: 24,
-          },
-        },
-        ticks: {
-          font: {
-            size: 18,
-          },
-        },
-        grid: {
-          drawOnChartArea: true,
-        },
-      },
-    },
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        labels: {
-          font: {
-            size: 20,
-          },
-        },
-        onClick: (e, legendItem) => {
-          const index = legendItem.datasetIndex;
-          const meta = e.chart.getDatasetMeta(index);
-          meta.hidden = !meta.hidden;
-          e.chart.update();
-        },
-      },
-      tooltip: {
-        backgroundColor: 'rgba(0, 0, 0, 1.0)',
-        titleFont: {
-          size: 16,
-        },
-        bodyFont: {
-          size: 14,
-        },
-        padding: 10,
-        boxPadding: 5,
-        cornerRadius: 4,
-      }
-    },
-  };
+	const options = {
+		scales: {
+			x: {
+				display: true,
+				title: {
+					type: 'time',
+					font: {
+						size: 24,
+					},
+				},
+				ticks: {
+					font: {
+						size: 18,
+					},
+					maxTicksLimit: Math.ceil(filteredLabels.length),
+				},
+			},
+			
+			'y-axis-1': {
+				type: 'linear',
+				display: true,
+				position: 'left',
+				title: {
+					display: true,
+					text: 'Temperature / Dew Point (°F)',
+					font: {
+						size: 24,
+					},
+				},
+				ticks: {
+					font: {
+						size: 18,
+					},
+				},
+			},
+			'y-axis-2': {
+				type: 'linear',
+				display: true,
+				position: 'right',
+				title: {
+					display: true,
+					text: 'Precipitation (%) / Wind Speed (mph)',
+					font: {
+						size: 24,
+					},
+				},
+				ticks: {
+					font: {
+						size: 18,
+					},
+				},
+				grid: {
+					drawOnChartArea: true,
+				},
+			},
+		},
+		responsive: true,
+		plugins: {
+			legend: {
+				display: true,
+				labels: {
+					font: {
+						size: 20,
+					},
+				},
+				onClick: (e, legendItem) => {
+					const index = legendItem.datasetIndex;
+					const meta = e.chart.getDatasetMeta(index);
+					meta.hidden = !meta.hidden;
+					e.chart.update();
+				},
+			},
+			tooltip: {
+				backgroundColor: 'rgba(0, 0, 0, 1.0)',
+				titleFont: {
+					size: 16,
+				},
+				bodyFont: {
+					size: 14,
+				},
+				padding: 10,
+				boxPadding: 5,
+				cornerRadius: 4,
+			}
+		},
+	};
 
-  return (
-    <div className={styles.weatherGraphContainer}>
-      <Box sx={{ width: '75%', textAlign: 'center'}}>
-        <Typography className={styles.weatherGraphTypography} gutterBottom>Day Range</Typography>
-        <Slider
-          value={dayRange}
-          onChange={handleSliderChange}
-          min={1}
-          max={7}
-          step={1}
-		      slotProps={{ markLabel: { style: { fontSize: '1.5rem'}}}}
-          marks={Array.from({ length: 7 }, (_, i) => ({
-            value: i + 1,
-            label: format(new Date(labels[i * 24]), 'EEE dd'),
-          }))}
-        />
-      </Box>
-      <Typography className={styles.weatherGraphLegendInstruction}>Click on the appropriate-colored box below to remove or add the line from the graph</Typography>
-      <Line data={dataConfig} options={options} />
-    </div>
-  );
+	return (
+		<div className={styles.weatherGraphContainer}>
+			<Box sx={{ width: '75%', textAlign: 'center'}}>
+				<Typography className={styles.weatherGraphTypography} gutterBottom>Day Range</Typography>
+				<Slider
+					value={dayRange}
+					onChange={handleSliderChange}
+					min={1}
+					max={7}
+					step={1}
+					slotProps={{
+						markLabel: { 
+							style: { 
+								fontSize: '1.5rem'
+							}
+						}
+					}}
+					marks={Array.from({ length: 7 }, (_, i) => ({
+						value: i + 1,
+						label: format(new Date(labels[i * 24]), 'EEE dd'),
+					}))}
+				/>
+			</Box>
+			<Typography className={styles.weatherGraphLegendInstruction}>Click on the appropriate-colored box below to remove or add the line from the graph</Typography>
+			<Line data={dataConfig} options={options} />
+		</div>
+	);
 };
 
 export default WeatherGraph;
